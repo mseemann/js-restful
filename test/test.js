fc02ff41d2b3f3efcf5251c84cbf8e2aa1faea7b
Michael Seemann <seemann@mseemann.de>
2016-04-28 17:56:18 +0200

    test the module structure

--- test/test.js
+++ test/test.js
@@ -1,3 +1,3 @@
-var example = require('../dist/js-restful');
-console.log(example);
-console.log(example.test);
+var jsRestful = require('../dist/js-restful');
+console.log(jsRestful);
+console.log(jsRestful.GET());
