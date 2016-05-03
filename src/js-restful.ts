c0c5f57d7da1a5c4cfbe6dbace8c6104f2ada967
Michael Seemann <seemann@mseemann.de>
2016-05-03 11:49:29 +0200

    export context

--- src/js-restful.ts
+++ src/js-restful.ts
@@ -1,5 +1,5 @@
-export { GET, POST, PUT, DELETE, Path, PathParam, HeaderParam, QueryParam } from './core/decorators';
+export { GET, POST, PUT, DELETE, Path, PathParam, HeaderParam, QueryParam, Context } from './core/decorators';
 export { ServiceParser } from './core/decorator-parser';
-export { ServiceDescription, MethodDescription, HttpMethod, ParamDescription } from './core/descriptions';
+export { ServiceDescription, MethodDescription, HttpMethod, ParamDescription, ContextTypes } from './core/descriptions';
 
 
