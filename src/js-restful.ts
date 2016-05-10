07cb079f5834f200681c4041e332539ed9182e0d
Michael Seemann <seemann@mseemann.de>
2016-05-10 14:05:54 +0200

    publish decorators for security

--- src/js-restful.ts
+++ src/js-restful.ts
@@ -1,4 +1,16 @@
-export { GET, POST, PUT, DELETE, Path, PathParam, HeaderParam, QueryParam, Context } from './core/decorators';
+export {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    Path,
+    PathParam,
+    HeaderParam,
+    QueryParam,
+    Context,
+    PermitAll,
+    SecurityContext,
+    RolesAllowed } from './core/decorators';
 export { ServiceParser } from './core/decorator-parser';
 export { ServiceDescription, MethodDescription, HttpMethod, ParamDescription, ContextTypes } from './core/descriptions';
 export { DecoratorUtil } from './core/decoratorUtil';
